6c39588d02d337b47caa9a33651aea67195039ae
Jean Rebello <jeanrebllo@gmai.com>
2024-01-06 20:02:40 -0300

    Html Reorganizado, falta o css

--- template.py
+++ template.py
@@ -11,8 +11,8 @@ from graphs import create_bar_figure, create_pie_figure
 def add_table_row():
     global table_data
     new_row = {'Id': len(table_data) + 1,
-               'Categoria': random.choice(palavras_aleatorias),
-               'Quantia': np.random.randint(100, 500)}
+            'Categoria': random.choice(palavras_aleatorias),
+            'Quantia': np.random.randint(100, 500)}
     # Alteração correta
     table_data = pd.concat([table_data, pd.DataFrame([new_row])], ignore_index=True)
 
@@ -22,7 +22,35 @@ for _ in range(10):
 def template():
     return html.Div(children=[
         html.H1('Controle de Gastos Pessoal', className='title'),
+        
+        #Div Geral
         html.Div([
+            
+            #Valores totais digitais
+            html.Div([
+                html.Div([
+                    html.Label('Renda Mensal Total: ', style={'color': '#ffff'}, className='renda-total'),
+                    html.Label('R$ 2583,96', style={'color': '#ffff'}, className='valor-renda'),
+                    html.Br(),
+                    html.Label('Total Gastos Mensais: ', style={'color': '#fff'}, className='renda-total'),
+                    html.Label('R$ 1569,63', style={'color': '#fff'}, className='valor-renda'),
+                    html.Br(),
+                    html.Label('Total Saldo: ', style={'color': '#ffff'}, className='renda-total'),
+                    html.Label('R$ 1014,33', style={'color': '#fff'}, className='valor-renda')
+                    ]),
+                ], className='table-container-value'),
+            html.Div([
+                html.Div([
+                #Grafico de Barra de Sabão
+                dcc.Graph(id='graph1', figure=create_bar_figure(categories, data, colors)),
+                ], className='graphs-bar'),
+                html.Div([
+                #Grafico de Pizza
+                dcc.Graph(id='graph2', figure=create_pie_figure(categories, data))
+                ], className='graphs-pizza'),
+            ], className='graphs-container'),
+            
+            #Tabela
             html.Div([
                 html.Div([
                     dash_table.DataTable(
@@ -33,17 +61,7 @@ def template():
                         style_cell={'textAlign': 'center'},  # Centraliza o texto nas células
                     )
                 ], className='table-container'),
-                html.Div([
-                    html.Div([
-                        html.Label('Renda Mensal Total: ', style={'color': '#ffff'}, className='renda-total'),
-                        html.Label('R$2583,96', style={'color': '#ffff'}, className='valor-renda'),
-                        html.Br(),
-                        html.Label('Total Gastos Mensais: ', style={'color': '#fff'}, className='renda-total'),
-                        html.Label('R$1569,63', style={'color': '#fff'}, className='valor-renda'),
-                        html.Br(),
-                        html.Label('Total Saldo: ', style={'color': '#ffff'}, className='renda-total'),
-                        html.Label('R$1014,33', style={'color': '#fff'}, className='valor-renda')
-                    ]),
+            #Inserção
                     dbc.Form([
                         html.Div([
                             html.Label("Inserir Nova Despesa", style={'color': '#ffff'}, className='receita-despesa2'),
@@ -68,15 +86,7 @@ def template():
                             ], className='receita-despesa'),
                             dbc.Button('Inserir', id='botao-categoria', color='primary', className='botao-inserir')
                         ])
-
-                    ])
-                ], className='table-container', style={'display': 'inline-block'})
-            ], className='div-tabela'),
-            html.Div([
-                dcc.Graph(id='graph1', figure=create_bar_figure(categories, data, colors)),
-                dcc.Graph(id='graph2', figure=create_pie_figure(categories, data))
-            ], className='graphs-container', style={'width': '50%', 'display': 'inline-block', 'vertical-align': 'top'}),
-
-        ], className='main-container', style={'display': 'flex', 'flex-direction': 'row'}),
-
+                    ], className='receita-geral'),
+                ], className='div-tabela'),
+        ], className='main-container'),
     ])
